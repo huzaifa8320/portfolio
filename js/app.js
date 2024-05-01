230ef31f6d52d6126c75d2dcb94d60570d87988d
huzaifa8320 <muhammadhuzaifa8320@gmailcom>
2024-05-01 21:31:33 +0500

    some changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
+// Pop up 
+
 const submitButton = document.getElementById('submit-button');
 const modal = document.getElementById('myModal');
 const closeButton = document.getElementById('close-button');
 
 // Show the modal when submit button is clicked
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', function () {
     modal.style.display = 'flex'; // Show the modal
 });
 
 // Close the modal when the close button is clicked
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function () {
     modal.style.display = 'none'; // Hide the modal
 });
 
 // Close the modal when clicking outside the content
-window.addEventListener('click', function(event) {
+window.addEventListener('click', function (event) {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
 });
+
+// Pop up end
